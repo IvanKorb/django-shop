8c1cf3c38a9eaf57ec2081dab2234bc89f240753
Ivan Korb <kor.ivan.1987@gmail.com>
2018-02-20 13:23:08 +0600

    Start project, add form,landing, models db

---
+++ landing/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from .forms import SubscriberForm
+
+# Create your views here.
+def landing(request):
+    name = "Ivan Kor"
+    current_day = "20.02.2018"
+    form = SubscriberForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        print(request.POST)
+        # print(form.cleaned_data)
+        # data = form.cleaned_data
+        # print(data["name"])
+
+        new_form = form.save()
+
+
+
+
+
+
+    return render(request, 'landing/landing.html', locals())
