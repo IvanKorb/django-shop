7a72efec2e57190f84602c340e2b25d99eb27b1f
Ivan Kor <kor_ivan@inbox.ru>
2018-02-22 13:19:58 +0600

    start poject, add admin panel, landing paage

--- landing/admin.py
+++ landing/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from .models import *
 
+
+class SubscriberAdmin(admin.ModelAdmin):
+    list_display = ["name", "email"]
+    # сортиротвка в админке
+    # list_filter = ['email']
+    # поиск в админке
+    search_fields = ['name', 'email']
+
+    fields = ['email']
+
+    class Meta:
+        model = Subscriber
+
+
 # Register your models here.
-admin.site.register(Subscribers)
+admin.site.register(Subscriber, SubscriberAdmin)
--- landing/models.py
+++ landing/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
+
 # Create your models here
-class Subscribers(models.Model):
+class Subscriber(models.Model):
     email = models.EmailField()
     name = models.CharField(max_length=128)
+
+    # вывод полей в админке
+    #def __str__(self):
+    #   return "Пользователь: %s Email: %s" % (self.name, self.email)
