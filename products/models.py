3729665482adf0bba77de6f6c7e3da5e3458cca3
Ivan Kor <kor_ivan@inbox.ru>
2018-02-26 11:30:12 +0600

    Override save method

--- products/models.py
+++ products/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=128)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     description = models.TextField(blank=True, null=True, default=None)
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
