50e8e4556bb45b060e6d7326e0be6956ce669d7f
Ivan Kor <kor_ivan@inbox.ru>
2018-03-05 12:30:04 +0600

    Add images product to home page

--- products/models.py
+++ products/models.py
@@ -13,7 +13,7 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True, default=None)
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
-    update = models.DateTimeField(auto_now_add=False, auto_now=True)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
     def __str__(self):
         return "Заказ %s" % self.name
@@ -25,11 +25,11 @@ class Product(models.Model):
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, blank=True, null=True, default=None)
-    image = models.ImageField(upload_to='product_images/')
+    image = models.ImageField(upload_to='products_images/')
     is_main = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
-    update = models.DateTimeField(auto_now_add=False, auto_now=True)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
     def __str__(self):
         return "Заказ %s" % self.id
